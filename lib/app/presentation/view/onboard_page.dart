b8a684c29a0d32d56e7ac716a01ef201dc3b7615
mfadlihs <winnerfadli999@gmail.com>
2024-05-24 19:06:23 +0700

    fix change google service

--- lib/app/presentation/view/onboard_page.dart
+++ lib/app/presentation/view/onboard_page.dart
@@ -4,6 +4,7 @@ import 'package:eport/app/presentation/widgets/app_button.dart';
 import 'package:eport/firebase_options.dart';
 import 'package:eport/styles/color_constants.dart';
 import 'package:eport/styles/text_styles.dart';
+import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -43,33 +44,73 @@ class _OnboardPageState extends State<OnboardPage> {
             ),
             AppButton(
               onPressed: () async {
-                // _googleSignIn.signIn().then((userData) {
-                //   // setState(() {
-                //   // });
-                //   print('cok anjay $userData');
-                // }).catchError((e) {
-                //   print(e);
+                // _googleSignIn.signIn().then((value) async {
+                //   final googleAuth = await value?.authentication;
+                //   final credential = GoogleAuthProvider.credential(
+                //     accessToken: googleAuth?.accessToken,
+                //     idToken: googleAuth?.idToken,
+                //   );
+                //   await auth.signInWithCredential(credential);
+                // }).catchError((err) {
+                //   print('errr $err');
+                //   err.printError();
                 // });
 
                 try {
-                  print('cok bisa cok');
-
                   final GoogleSignInAccount? googleUser =
                       await GoogleSignIn().signIn();
 
+                  // if (googleUser == null) {
+                  //   return;
+                  // }
+
                   final GoogleSignInAuthentication? googleAuth =
                       await googleUser?.authentication;
+                  // if (googleAuth == null) {
+                  //   return;
+                  // }
 
                   final credential = GoogleAuthProvider.credential(
                     accessToken: googleAuth?.accessToken,
                     idToken: googleAuth?.idToken,
                   );
                   await auth.signInWithCredential(credential);
-                } catch (e) {
+                } on Exception catch (e) {
                   print('exception->$e');
                 }
               },
               text: "Masuk",
+            ),
+            AppButton(
+              onPressed: () async {
+                try {
+                  await auth.signOut();
+                  await _googleSignIn.signOut();
+                } catch (e) {
+                  print('exception->$e');
+                }
+              },
+              text: "Logout",
+            ),
+            AppButton(
+              onPressed: () async {
+                try {
+                  final u = auth.currentUser;
+                  print(u);
+                } catch (e) {}
+              },
+              text: "cek akun",
+            ),
+            AppButton(
+              onPressed: () async {
+                try {
+                  final a = await auth.signInWithEmailAndPassword(
+                      email: "test+1@gmail.com", password: "password");
+
+                  print(a);
+                } catch (err) {}
+              },
+              text: "Login Email",
             ),
           ],
         ),
